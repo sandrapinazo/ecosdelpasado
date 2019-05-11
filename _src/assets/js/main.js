16fbb42add4d1ed465dce2ccee9e5485a7f9df0e
sandrapinazo <sandrapinazogonzalez@gmail.com>
2019-05-11 04:27:36 +0200

    Add update array with new news and update news page logic

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -1,12 +1,48 @@
 'use strict';
 
 const allNews = [
-  {location: 'Vaticano', date: 20190504, title: 'Más mujeres en la universidad', main: 'Repentino aumento de las matrículas de mujeres en la universidad del Vaticano. Es estima un 48% más de mujeres que el año pasado.'},
-  {location: 'Vaticano', date: 20190414, title: '¡Cuidado en las calles!', main: 'Recientes rumores acerca de un extraño de aspecto sospechoso recorriendo las calles en busca de hoja roja. Se recomienda alejarse de extraños y mantener a los niños acompañados.'},
-  {location: 'Poza Aguada', date: 20190407, title: 'Trágico fin de la expedición vodacce', main: 'La expedición vodacce que tenía intención de encontrar y explorar la tumba del Imperatus Gaius Filipus termina en tragedia tras el ataque de bandidos montaigneses. Los supervivientes, parte del equipo que acompañaba a Oscar Zurita y Carlo Grazia Caligari, regresó la pasada semana portando las noticias.'},
-  {location: 'Vaticano', date: 20190504, title: 'Más mujeres en la universidad', main: 'Repentino aumento de las matrículas de mujeres en la universidad del Vaticano. Es estima un 48% más de mujeres que el año pasado.'},
-  {location: 'Vaticano', date: 20190414, title: '¡Cuidado en las calles!', main: 'Recientes rumores acerca de un extraño de aspecto sospechoso recorriendo las calles en busca de hoja roja. Se recomienda alejarse de extraños y mantener a los niños acompañados.'},
-  {location: 'Poza Aguada', date: 20190407, title: 'Trágico fin de la expedición vodacce', main: 'La expedición vodacce que tenía intención de encontrar y explorar la tumba del Imperatus Gaius Filipus termina en tragedia tras el ataque de bandidos montaigneses. Los supervivientes, parte del equipo que acompañaba a Oscar Zurita y Carlo Grazia Caligari, regresó la pasada semana portando las noticias.'},
+  {
+    location: 'Vaticano',
+    date: '2019-05-04',
+    title: 'Más mujeres en la universidad',
+    main:
+      'Repentino aumento de las matrículas de mujeres en la universidad del Vaticano. Es estima un 48% más de mujeres que el año pasado.'
+  },
+  {
+    location: 'Vaticano',
+    date: '2019-04-14',
+    title: '¡Cuidado en las calles!',
+    main:
+      'Recientes rumores acerca de un extraño de aspecto sospechoso recorriendo las calles en busca de hoja roja. Se recomienda alejarse de extraños y mantener a los niños acompañados.'
+  },
+  {
+    location: 'Poza Aguada',
+    date: '2019-04-07',
+    title: 'Trágico fin de la expedición vodacce',
+    main:
+      'La expedición vodacce que tenía intención de encontrar y explorar la tumba del Imperatus Gaius Filipus termina en tragedia tras el ataque de bandidos montaigneses. Los supervivientes, parte del equipo que acompañaba a Oscar Zurita y Carlo Grazia Caligari, regresó la pasada semana portando las noticias.'
+  },
+  {
+    location: 'Vaticano',
+    date: '2019-05-04',
+    title: 'Más mujeres en la universidad',
+    main:
+      'Repentino aumento de las matrículas de mujeres en la universidad del Vaticano. Es estima un 48% más de mujeres que el año pasado.'
+  },
+  {
+    location: 'Vaticano',
+    date: '2019-04-14',
+    title: '¡Cuidado en las calles!',
+    main:
+      'Recientes rumores acerca de un extraño de aspecto sospechoso recorriendo las calles en busca de hoja roja. Se recomienda alejarse de extraños y mantener a los niños acompañados.'
+  },
+  {
+    location: 'Poza Aguada',
+    date: '2019-04-07',
+    title: 'Trágico fin de la expedición vodacce',
+    main:
+      'La expedición vodacce que tenía intención de encontrar y explorar la tumba del Imperatus Gaius Filipus termina en tragedia tras el ataque de bandidos montaigneses. Los supervivientes, parte del equipo que acompañaba a Oscar Zurita y Carlo Grazia Caligari, regresó la pasada semana portando las noticias.'
+  }
 ];
 const newsUlEl = document.querySelector('.news-ul');
 const locationsUlEl = document.querySelector('.locations-container');
@@ -14,11 +50,21 @@ const pageTitle = document.querySelector('.page-title');
 const createNewsEl = document.querySelector('.location-tab__create-news');
 const formEl = document.querySelector('.create-news--form');
 const formButtonEl = document.querySelector('.form-button');
+const formCloseEl = document.querySelector('.form-button-close');
+const inputEl = document.querySelectorAll('.form-input');
+let newNews = {};
 
-if (pageTitle.innerHTML==='News') {writeNews(allNews);}
-if (pageTitle.innerHTML==='News') {writeLocations();}
+console.log(allNews);
+if (pageTitle.innerHTML === 'News') {
+  writeNews(allNews);
+}
+if (pageTitle.innerHTML === 'News') {
+  writeLocations();
+}
 
-function writeNews (news) {
+function writeNews(news) {
+  newsUlEl.innerHTML = '';
+  //fetch news from MyJson api
   for (const item of news) {
     const newLi = document.createElement('li');
     newLi.classList.add('news-container');
@@ -44,51 +90,95 @@ function writeNews (news) {
   }
 }
 
-function writeLocations () {
+function writeLocations() {
+  locationsUlEl.innerHTML='';
   let allLocations = [];
   for (const item of allNews) {
-    if(!allLocations.includes(item.location)) {
+    if (!allLocations.includes(item.location)) {
       allLocations.push(item.location);
     }
   }
   allLocations.sort();
-  allLocations.splice(0,0,'Thea');
-  for (const location of allLocations){
+  allLocations.splice(0, 0, 'Thea');
+  for (const location of allLocations) {
     const newLi = document.createElement('li');
     const newLocation = document.createTextNode(location);
     newLi.appendChild(newLocation);
     newLi.classList.add('location-tab');
-    if (location === 'Thea') { newLi.classList.add('Thea'); }
+    if (location === 'Thea') {
+      newLi.classList.add('Thea');
+    }
     newLi.addEventListener('click', filterLocation);
     locationsUlEl.appendChild(newLi);
   }
 }
 
-function filterLocation (event) {
+function filterLocation(event) {
   const selectedLocation = event.currentTarget.innerHTML;
-  const locationNews = allNews.filter(item => (selectedLocation==='Thea')? item.location : item.location === selectedLocation);
+  const locationNews = allNews.filter(item =>
+    selectedLocation === 'Thea'
+      ? item.location
+      : item.location === selectedLocation
+  );
   clearNews();
   writeNews(locationNews);
   const locationTabs = locationsUlEl.querySelectorAll('.location-tab');
-  for (const tab of locationTabs){
+  for (const tab of locationTabs) {
     tab.classList.remove('selected-location-tab');
   }
   event.currentTarget.classList.add('selected-location-tab');
 }
 
-function clearNews () {
+function clearNews() {
   newsUlEl.innerHTML = '';
 }
 
-
-function showForm () {
+function showForm() {
   formEl.classList.remove('hidden');
 }
 
 createNewsEl.addEventListener('click', showForm);
 
-function handleCreateNews () {
-  formEl.classList.add('hidden');
+function handleCreateNews() {
+  //check if all inputs are filled
+  const filledInputs = checkInputsFilled();
+  if (filledInputs) {
+    getValueFromInputs();
+    console.log(newNews);
+    //add newNews to allNews
+    allNews.push(newNews);
+    //parse allNews to Json
+    //send Json to myJson
+    //update news and locations again
+    writeNews(allNews);
+    writeLocations();
+    closeForm();
+  } else {
+    alert('Rellena todos los campos');
+  }
 }
 
-formButtonEl.addEventListener('click',handleCreateNews);
+formButtonEl.addEventListener('click', handleCreateNews);
+
+function getValueFromInputs() {
+  for (const input of inputEl) {
+    const key = input.id;
+    newNews[key] = input.value;
+  }
+}
+
+function closeForm() {
+  formEl.classList.add('hidden');
+  newNews = {};
+}
+
+formCloseEl.addEventListener('click', closeForm);
+
+function checkInputsFilled() {
+  for (const input of inputEl) {
+    if (!input.value) {
+      return false;
+    }
+  }
+  return true;
+}
